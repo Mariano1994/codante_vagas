d6ae4b5ea1d7c6eb55bdd9542daf6286b66f967c
Mariano Capiliku <marianocapiliku@gmail.com>
2025-04-23 19:06:59 +0100

    styled maintanence page

--- app/(marketing)/manutencao/page.tsx
+++ app/(marketing)/manutencao/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import maintenanceImage from "../../assets/maintenance-image.svg";
+
 export default function ManutencaoPage() {
   return (
-    <div>
-      <p>Estamos em Manutencao</p>
+    <div className=" flex flex-col items-center justify-center gap-8 my-12">
+      <h1 className=" text-2xl font-medium"> Website em Manutenção</h1>
+      <Image
+        src={maintenanceImage}
+        alt="Imagem de manutencao"
+        className="w-[550px]"
+      />
+
+      <p className="text-xl font-thin text-gray-500">
+        Estamos em manutencao no momento, por favor volte mais tarde
+      </p>
     </div>
   );
 }
